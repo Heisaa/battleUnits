19da7198df522e9766ce3f17a75c2aa4a1653f06
heisaa <henriksandhall@gmail.com>
2019-04-22 00:28:23 +0200

    selection

--- scripts/CameraMovement.gd
+++ scripts/CameraMovement.gd
@@ -1,20 +1,25 @@
 extends Camera2D
 
+export var panSpeed = 20.0
 export var speed = 20.0
 export var zoomSpeed = 10.0
-export var zoomSmoothing = 0.7
+export var zoomSmoothing = 0.1
 
 export var zoomMin = 0.25
 export var zoomMax = 3.0
+export var marginX = 10.0
+export var marginY = 10.0
 
-
+var mousePos = Vector2()
 var zoomPos = Vector2()
 var zoomFactor = 1.0
 var zooming = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position.x = get_viewport().size.x/2
+	position.y = get_viewport().size.y/2
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -30,6 +35,17 @@ func _process(delta):
 	position.y = lerp(position.y, 
 	                  position.y + inputY * speed * zoom.y, 
 	                  speed * delta)
+	
+	# Edge panning
+	mousePos = get_viewport().get_mouse_position()
+	if mousePos.x < marginX:
+		position.x = lerp(position.x, position.x - panSpeed * zoom.x, panSpeed * delta)
+	elif mousePos.x > OS.window_size.x - marginX:
+		position.x = lerp(position.x, position.x + panSpeed * zoom.x, panSpeed * delta)
+	if mousePos.y < marginY:
+		position.y = lerp(position.y, position.y - panSpeed * zoom.y, panSpeed * delta)
+	elif mousePos.y > OS.window_size.y - marginY:
+		position.y = lerp(position.y, position.y + panSpeed * zoom.y, panSpeed * delta)
 	
 	# Zoom in
 	zoom.x = lerp(zoom.x, zoom.x * zoomFactor, zoomSpeed * delta)
---
+++ scripts/Unit.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+export var selected = false setget setSelected
+onready var box = $box
+onready var label = $label
+onready var bar = $bar
+
+
+signal wasSelected
+signal wasDeselected
+
+func setSelected(value):
+	if selected != value:
+		selected = value
+		box.visible = value
+		if selected:
+			emit_signal("wasSelected", self)
+		else:
+			emit_signal("wasDeselected", self)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("wasSelected", get_parent(),"selectUnit")
+	connect("wasDeselected",get_parent(),"deselectUnit")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_unit_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_LEFT:
+				setSelected(true)
+			if event.button_index == BUTTON_RIGHT:
+				setSelected(false)
